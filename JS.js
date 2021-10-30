0841dfb26fa7da543f3d2c75da844cf2bfd2fbf1
MrTQJ <qtan033@e.ntu.edu.sg>
2021-10-30 21:58:49 +0800

    contact us & faq

--- JS.js
+++ JS.js
@@ -41,3 +41,24 @@ function showSlides(n) {
   slides[slideIndex-1].style.display = "block";
   dots[slideIndex-1].className += " active";
 }
+
+//faq//
+
+var faq = document.getElementsByClassName("faqsection");
+var i;
+
+for (i = 0; i < faq.length; i++) {
+  faq[i].addEventListener("click", function() {
+    /* Toggle between adding and removing the "active" class,
+    to highlight the button that controls the panel */
+    this.classList.toggle("active");
+
+    /* Toggle between hiding and showing the active panel */
+    var panel = this.nextElementSibling;
+    if (panel.style.display === "block") {
+      panel.style.display = "none";
+    } else {
+      panel.style.display = "block";
+    }
+  });
+}
